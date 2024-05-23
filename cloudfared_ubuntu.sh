87bb083d817cd7c9094af881831ba696a27f929c
Sunny <syslogbd@gmail.com>
2024-05-24 04:38:10 +0600

    Create cloudfared_ubuntu.sh

---
+++ cloudfared_ubuntu.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiMDUzMzNhODQ0YzhiN2ZiNGQ0ODZmZWZkOGVkMTBmOGUiLCJ0IjoiYzhmNDQzNjYtNDljNy00NDRjLWE4ZWItMGZhODYwZjE4OTcyIiwicyI6Ik5XVXpaalEzWVRVdE4yRTJOQzAwWW1ZMUxXSTVOMlV0T0RCaE9UUm1OV0kwTnpFNSJ9
