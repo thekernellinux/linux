a2910aed8da00430a87a323970f9d86b54fecc3b
Sunny <syslogbd@gmail.com>
2024-07-05 00:07:13 +0600

    Update add_user_sysadmin.sh

--- add_new_user.sh
+++ add_new_user.sh
@@ -3,9 +3,9 @@
 # Add user if not exists
 if ! id "sysadmin" &>/dev/null; then
     useradd -m -s /bin/bash newuser
-    echo "newuser:password" | chpasswd
-    usermod -aG sudo newuser
-    echo "User newuser created and added to sudo group."
+    echo "sysadmin:password" | chpasswd
+    usermod -aG sudo sysadmin
+    echo "User sysadmin created and added to sudo group."
 else
-    echo "User newuser already exists."
+    echo "User sysadmin already exists."
 fi
