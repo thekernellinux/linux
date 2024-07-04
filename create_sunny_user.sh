b9dffeeb0f6ee1429624afb25f031f7cad7b3825
Sunny <syslogbd@gmail.com>
2024-07-04 22:44:30 +0600

    Update create_sunny_user.sh

---
+++ create_sunny_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if user exists
+if id "sunny" &>/dev/null; then
+    echo "User sunny already exists."
+else
+    # Create user sunny
+    sudo adduser --quiet --disabled-password --gecos "" sunny
+
+    # Set password for user sunny
+    echo "sunny:Dhaka-Metro-24" | sudo chpasswd
+
+    # Add user sunny to sudo group with NOPASSWD:ALL permissions
+    echo "sunny ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/sunny
+fi
